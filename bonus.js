48e3d8fe7253d4fdbb939e7ee8a25d2c1d414caa
jordancclive <jordancclive@gmail.com>
2017-05-16 11:47:40 -0400

    fixed several bugs

--- bonus.js
+++ bonus.js
@@ -20,7 +20,7 @@ const genEnumArr = (str)=>{
   // takes a string formatted like the provided example and makes a workable, neat array out of it
   let arr = neatArray(str);
   let newArr = [];
-  let generation = -1;
+  let generation = 0;
   // replaces all parenthesis with generation numbers and adds generation numbers before every work not surrounded with parentheses
   for (var i=0; i<arr.length; i++){
     if (arr[i]==='('){
@@ -40,12 +40,8 @@ const genEnumArr = (str)=>{
 const alphaSort = (str)=>{
   let arr = genEnumArr(str);
 
-  // IDENTIFIES PARENT OF EACH GENERATION (FUNCTION FOUND BELOW)
-  let gen2Parent = parentIdentifier(arr, gen1, 1);
-  let gen3Parent = parentIdentifier(arr, gen2, 2);
-
   // SEPARATES GENERATIONS 0, 1, AND 2 AND REMOVES ENUMERATION (FUNCTION FOUND BELOW)
-  let genSep = genSeparator(arr, gen2, gen3);
+  let genSep = genSeparator(arr);
   let gen1 = genSep[0];
   let gen2 = genSep[1];
   let gen3 = genSep[2];
@@ -54,6 +50,10 @@ const alphaSort = (str)=>{
   gen1.sort();
   gen2.sort();
   gen3.sort();
+
+  // IDENTIFIES PARENT OF EACH GENERATION (FUNCTION FOUND BELOW)
+  let gen2Parent = parentIdentifier(arr, gen2, 2);
+  let gen3Parent = parentIdentifier(arr, gen3, 3);
 
   // ADDS GENERATION ARRAYS TO gen1 AND ADDS ENUMERATION (FUNCTION FOUND BELOW)
   const gen2ParentNum = gen1.indexOf(gen2Parent)
@@ -75,28 +75,30 @@ const parentIdentifier = (arr, gen, num)=>{
   return gen.parent;
 }
 
-const genSeparator = (arr, gen2, gen3)=>{
+const genSeparator = (arr)=>{
   let gen1 = [];
+  let gen2 = [];
+  let gen3 = [];
   for (var i=0; i<arr.length; i++){
     if (typeof arr[i]==='number'){
       continue;
     }
-    if (arr[i-1]===0){
+    if (arr[i-1]===1){
       gen1.push(arr[i]);
     }
-    if (arr[i-1]===1){
-      gen2.siblings.push(arr[i])
-    }
     if (arr[i-1]===2){
-      gen3.siblings.push(arr[i])
+      gen2.push(arr[i])
+    }
+    if (arr[i-1]===3){
+      gen3.push(arr[i])
     }
   }
-  return [newArr, gen1, gen2];
+  return [gen1, gen2, gen3];
 }
 
 const addGeneration= (arr, genparent, gen, num)=>{
-  for (var i=gen.siblings.length-1; i>=0; i--){
-    arr.splice(genparent+1, 0, num, gen.siblings[i]);
+  for (var i=gen.length-1; i>=0; i--){
+    arr.splice(genparent+1, 0, num, gen[i]);
   }
   return arr;
 }
@@ -104,8 +106,7 @@ const addGeneration= (arr, genparent, gen, num)=>{
 // GENERATES OUTPUT STRING
 
 function outputStr(str){
-  // takes a string formatted like the provided example and outputs an array with generation numbers preceding all cells with words
-  let arr = genEnumArr(str);
+  let arr = alphaSort(str);
   let newStr = '';
   // produces a string in the desired output format by putting as many dashes as the generation of a word in front of it and a line break after each word
   for (var i=0; i<arr.length; i++){
@@ -113,11 +114,7 @@ function outputStr(str){
       newStr+=arr[i]+'\n';
     }
     if (typeof arr[i]==='number'){
-      if (arr[i]===0){
-        continue;
-      } else {
-        newStr+='-'.repeat(arr[i])+' ';
-      }
+      newStr+='-'.repeat(arr[i]-1)+' ';
     }
   }
   return newStr;
